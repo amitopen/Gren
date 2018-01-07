2d63cfa05473333621f46ea1adc7c45d7f29e569
amitopen <amitopen007@gmail.com>
2018-01-07 08:45:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dream/gre/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.dream.gre;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.content,new  HomeFragment() ).commit();
+                    getSupportActionBar().setTitle("Gre");
+                    getSupportActionBar().show();
+                    return true;
+                case R.id.navigation_study:
+                    transaction.replace(R.id.content,new StudyFragment()).commit();
+                    getSupportActionBar().setTitle("Study");
+                    getSupportActionBar().show();
+                    return true;
+                case R.id.navigation_profile:
+                    getSupportActionBar().setTitle("Profile");
+                    getSupportActionBar().show();
+                    transaction.replace(R.id.content,new ProfileFragment()).commit();
+                    return true;
+                case R.id.navigation_more:
+                    transaction.replace(R.id.content,new MoreFragment()).commit();
+                    getSupportActionBar().hide();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content,new HomeFragment()).commit();
+
+    }
+
+    private void actionBar() {
+        View view = getLayoutInflater().inflate(R.layout.action_bar, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER);
+
+        TextView Title = (TextView) view.findViewById(R.id.actionbar_title);
+        Title.setText("Home");
+
+        getSupportActionBar().setCustomView(view,params);
+        getSupportActionBar().setDisplayShowCustomEnabled(true); //show custom title
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //hide the default title
+    }
+}
